722a62f16e292c0bbf7ab51567e0ae50753d20d9
Александр <7176uiop3436>
2023-05-10 02:44:54 +0300

    [feat] swagger in a process

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,31 @@ import (
 	"BookStore/internal/controllers/books"
 	"BookStore/internal/controllers/users"
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	//_ "github.com/xanderfrost/bookstore/docs/bookstore"
+
+	"log"
+	"net/http"
 	"os"
 )
+
+// @title Gin Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
 
 func main() {
 	r := gin.Default()
@@ -21,6 +44,32 @@ func main() {
 	// Извлекаем переменную окружения PORT
 	port := os.Getenv("PORT")
 
+	// Routes
+	r.GET("/", HealthCheck)
+	// The url pointing to API definition
+	url := ginSwagger.URL("http://localhost:3000/swagger/doc.json")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
 	// Запускаем сервер на указанном порту
-	r.Run(port)
+	if err := r.Run(port); err != nil {
+		log.Fatal(err)
+	}
+
+	// HealthCheck godoc
+	// @Summary Show the status of server.
+	// @Description get the status of server.
+	// @Tags root
+	// @Accept */*
+	// @Produce json
+	// @Success 200 {object} map[string]interface{}
+	// @Router / [get]
+
+}
+
+func HealthCheck(c *gin.Context) {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	c.JSON(http.StatusOK, res)
 }
